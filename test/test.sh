a0e5f297c1e7b92c0fa88f391706e55c65a7d1a3
Johannes Milczewski <johannes.milczewski@ocead.de>
2021-11-20 01:25:14 +0100

    Added tests for timestamp generation

--- test/test.sh
+++ test/test.sh
@@ -1,20 +1,60 @@
 #!/usr/bin/env bash
 
+# Print an error
 function echo_error() {
 	echo >&2 -e "[\e[0;31mERROR\e[0m]: $1"
+	exit 254
 }
 
-function verify_actual() {
-	git checkout "master"
+# Adds a TSA configuration to the repository
+function add_tsa() {
+	git checkout "sig-"
+	mkdir "./rfc3161/zeitstempel.dfn.de"
+	curl "https://pki.pca.dfn.de/dfn-ca-global-g2/pub/cacert/chain.txt" -o "./rfc3161/zeitstempel.dfn.de/cacert.pem"
+	git add "./rfc3161/zeitstempel.dfn.de/cacert.pem"
+	git commit -m "Added simple TSA" "./rfc3161/zeitstempel.dfn.de/cacert.pem"
+
+	# Checkout original branch
+  if git rev-parse --verify "master" >/dev/null 2>/dev/null; then
+  	git checkout "master" >/dev/null 2>/dev/null
+  else
+  	git checkout --orphan "master" >/dev/null 2>/dev/null
+  	git rm -rf ./rfc3161/* >/dev/null 2>/dev/null
+  fi
+}
+
+# Verifies the repository after first actual commit
+function verify_first() {
+	git checkout "master" >/dev/null 2>/dev/null
 	! test -d rfc3161 || echo_error "rfc3161/ should not exist."
 	! test -f .diff || echo_error ".diff should not exist."
 	test -f a.txt || echo_error "a.txt should exist."
 
-	if git rev-parse --verify "sig/master" 2>/dev/null; then
-		git checkout "sig/master"
+	if git rev-parse --verify "sig/master" >/dev/null 2>/dev/null; then
+		git checkout "sig/master" >/dev/null 2>/dev/null
 		test -d rfc3161/ || echo_error "rfc3161/ should exist."
 		! test -f a.txt 2>/dev/null || echo_error "a.txt should not exist."
 	fi
+
+	git checkout "master" >/dev/null 2>/dev/null
+}
+
+# Verifies the repository after second actual commit
+function verify_second() {
+	git checkout "master" >/dev/null 2>/dev/null
+	! test -d rfc3161 || echo_error "rfc3161/ should not exist."
+	! test -f .diff || echo_error ".diff should not exist."
+	test -f a.txt || echo_error "a.txt should exist."
+	test -f b.txt || echo_error "b.txt should exist."
+
+	if git rev-parse --verify "sig/master" >/dev/null 2>/dev/null; then
+		git checkout "sig/master" >/dev/null 2>/dev/null
+		test -d rfc3161/ || echo_error "rfc3161/ should exist."
+		! test -f a.txt 2>/dev/null || echo_error "a.txt should not exist."
+		! test -f b.txt 2>/dev/null || echo_error "b.txt should not exist."
+	fi
+
+	git checkout "master" >/dev/null 2>/dev/null
 }
 
 # Install to empty repository
@@ -28,11 +68,18 @@ git --git-dir="${REPO_PATH}/.git" init
 ./config.sh -d "${REPO_PATH}"
 (
 	cd "${REPO_PATH}" || exit 255
+	add_tsa
 	echo "Gnampf" >"./a.txt"
-	git add "./a.txt"
+	git add "./a.txt" >/dev/null 2>/dev/null
 	git commit -m "Initial commit"
 
-	verify_actual
+	verify_first
+
+	echo "Schnampf" >"./b.txt"
+	git add "./b.txt" >/dev/null 2>/dev/null
+	git commit -m "Second commit"
+
+	verify_second
 )
 echo ""
 
@@ -48,10 +95,17 @@ git --git-dir="${REPO_PATH}/.git" init
 ./config.sh --default "${REPO_PATH}"
 (
 	cd "${REPO_PATH}" || exit 255
+	add_tsa
 	git add "./a.txt"
 	git commit -m "Initial commit"
 
-	verify_actual
+	verify_first
+
+	echo "Schnampf" >"./b.txt"
+	git add "./b.txt"
+	git commit -m "Second commit"
+
+	verify_second
 )
 echo ""
 
@@ -68,9 +122,16 @@ git --git-dir="${REPO_PATH}/.git" init
 ./config.sh -d "${REPO_PATH}"
 (
 	cd "${REPO_PATH}" || exit 255
+	add_tsa
 	git commit -m "Initial commit"
 
-	verify_actual
+	verify_first
+
+	echo "Schnampf" >"./b.txt"
+	git add "./b.txt"
+	git commit -m "Second commit"
+
+	verify_second
 )
 echo ""
 
@@ -88,8 +149,15 @@ git --git-dir="${REPO_PATH}/.git" init
 ./config.sh --default "${REPO_PATH}"
 (
 	cd "${REPO_PATH}" || exit 255
+	add_tsa
 
-	verify_actual
+	verify_first
+
+	echo "Schnampf" >"./b.txt"
+	git add "./b.txt"
+	git commit -m "Second commit"
+
+	verify_second
 )
 echo ""
 
@@ -109,8 +177,17 @@ git --git-dir="${REPO_PATH}/.git" init
 ./config.sh --default "${REPO_PATH}"
 (
 	cd "${REPO_PATH}" || exit 255
+	add_tsa
 
-	verify_actual
-	test -f b.txt || echo_error "a.txt should exist."
+	verify_first
+	test -f b.txt || echo_error "b.txt should exist."
+
+
+
+	echo "Schnampf" >"./b.txt"
+	git add "./b.txt"
+	git commit -m "Second commit"
+
+	verify_second
 )
 echo ""
